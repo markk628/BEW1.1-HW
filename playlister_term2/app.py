1fb7147e90f534c1cdfe986abacdbbd8ffa6e117
Donghyun <markk628@gmail.com>
2019-11-30 23:01:13 -0800

    users can see playlists

--- playlister_term2/app.py
+++ playlister_term2/app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+playlists = [
+    { 'title': 'Dog Videos', 'description': 'Dogs >>>>> Cats' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Coolio!?')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
 
 if __name__ == '__main__':
     app.run(debug=True)
