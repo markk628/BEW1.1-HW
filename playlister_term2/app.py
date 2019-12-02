807cca6866a7f4f988fde7c3de6c3280c6fcb85b
Donghyun <markk628@gmail.com>
2019-12-02 14:37:13 -0800

    added mongodb

--- playlister_term2/app.py
+++ playlister_term2/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 
 app = Flask(__name__)
 
-playlists = [
-    { 'title': 'Dog Videos', 'description': 'Dogs >>>>> Cats' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+# playlists = [
+#     { 'title': 'Dog Videos', 'description': 'Dogs >>>>> Cats' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+# ]
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
