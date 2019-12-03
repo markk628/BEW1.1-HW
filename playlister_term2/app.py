084831fb6661787356080cd7debed1b9853f8ac1
Donghyun <markk628@gmail.com>
2019-12-03 00:29:46 -0800

    added created_at, footer, and bootstrap theme

--- playlister_term2/app.py
+++ playlister_term2/app.py
@@ -1,14 +1,18 @@
+
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
 
-client = MongoClient()
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+
+client = MongoClient(host=f'{host}?retryWrites=false')  # Mongo client
+app = Flask(__name__)  # Flask app
+
 db = client.get_default_database()
 playlists = db.playlists
-# Add this line:
 comments = db.comments
 
-app = Flask(__name__)
 
 def video_url_creator(id_lst):
     videos = []
@@ -17,11 +21,6 @@ def video_url_creator(id_lst):
         video = 'https://youtube.com/embed/' + vid_id
         videos.append(video)
     return videos
-
-# playlists = [
-#     { 'title': 'Dog Videos', 'description': 'Dogs >>>>> Cats' },
-#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-# ]
 
 @app.route('/')
 def playlists_index():
@@ -33,7 +32,6 @@ def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html')
 
-
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
     """Submit a new playlist."""
@@ -41,15 +39,18 @@ def playlists_submit():
     video_ids = request.form.get('video_ids').split()
     # call our helper function to create the list of links
     videos = video_url_creator(video_ids)
+    
     playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': videos,
-        'video_ids': video_ids
+        'video_ids': video_ids,
+        'created_at': datetime.now()
     }
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+    
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
     """Show a single playlist."""
@@ -57,28 +58,31 @@ def playlists_show(playlist_id):
     playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
-
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
     """Submit an edited playlist."""
     video_ids = request.form.get('video_ids').split()
     videos = video_url_creator(video_ids)
+    # create our updated playlist
     updated_playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': videos,
         'video_ids': video_ids
     }
+    # set the former playlist to the new one we just updated/edited
     playlists.update_one(
         {'_id': ObjectId(playlist_id)},
         {'$set': updated_playlist})
     # take us back to the playlist's show page
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    # Add the title parameter here
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
 
 @app.route('/playlists/<playlist_id>/delete', methods=['POST'])
 def playlists_delete(playlist_id):
@@ -89,6 +93,8 @@ def playlists_delete(playlist_id):
 @app.route('/playlists/comments', methods=['POST'])
 def comments_new():
     """Submit a new comment."""
+    # TODO: Fill in the code here to build the comment object,
+    # and then insert it into the MongoDB comments collection
     comment = {
         'title': request.form.get('title'),
         'content': request.form.get('content'),
@@ -98,6 +104,12 @@ def comments_new():
     comment_id = comments.insert_one(comment).inserted_id
     return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
